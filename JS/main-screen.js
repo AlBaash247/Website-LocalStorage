eface368e8427994e79158d7ca86462c63744b7d
AlineJo <albaash247>
2023-07-01 15:50:03 +0400

    + main screen

---
+++ JS/main-screen.js
@@ -0,0 +1,23 @@
+import {
+    KEY_LOCAL_STORAGE_ROLE,
+    KEY_LOCAL_STORAGE_EMAIL,
+    KEY_LOCAL_STORAGE_PASSWORD
+} from './KEYs.js';
+
+displayContent()
+
+function displayContent() {
+
+    let txtRole = document.getElementById("txtRole")
+    let txtEmail = document.getElementById("txtEmail")
+    let txtPassword = document.getElementById("txtPassword")
+
+    let role = localStorage.getItem(KEY_LOCAL_STORAGE_ROLE)
+    let email = localStorage.getItem(KEY_LOCAL_STORAGE_EMAIL)
+    let password = localStorage.getItem(KEY_LOCAL_STORAGE_PASSWORD)
+
+    txtRole.innerText += " " + role
+    txtEmail.innerText += " " + email
+    txtPassword.innerText += " " + password
+}
+
