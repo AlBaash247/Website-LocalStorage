a8c48baf6d473fafd537495cadf649d3d9f34fc2
AlineJo <albaash247>
2023-07-01 15:49:51 +0400

    + keys file

---
+++ JS/KEYs.js
@@ -0,0 +1,7 @@
+export const KEY_APP_ROLE_ADMIN  = "Admin"
+export const KEY_APP_ROLE_VENDOR = "Vendor"
+export const KEY_APP_ROLE_USER   = "Client"
+
+export const KEY_LOCAL_STORAGE_ROLE = "role"
+export const KEY_LOCAL_STORAGE_EMAIL = "email"
+export const KEY_LOCAL_STORAGE_PASSWORD = "password"
